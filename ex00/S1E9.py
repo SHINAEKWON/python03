d9439c6f5ae1e108b6bef77b3c8fcd8cc788dbab
Shin ae KWON <shikwon@f4r14s14.clusters.42paris.fr>
2025-07-08 19:13:47 +0200

    hahaha ex00 done

--- ex00/S1E9.py
+++ ex00/S1E9.py
@@ -2,11 +2,33 @@ from abc import ABC, abstractmethod
 
 
 class Character(ABC):
-"""Your docstring for Class"""
-@abstractmethod
-#your code here
+    """
+    Character Class
+    """
+    def __init__(self, name, is_alive=True):
+        """
+        Constructeor for Character class
+        Parameters:
+            name(str): Name of the character
+            is_alive(bool): life status of the character (default: True)
+        """
+        self.name = name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self):
+
+        pass
 
 
 class Stark(Character):
-"""Your docstring for Class"""
-#your code here
+    """
+    Stark Class herits Character class
+    """
+
+    def die(self):
+        """
+        This change the character's status to
+        is_alive: False
+        """
+        self.is_alive = False
